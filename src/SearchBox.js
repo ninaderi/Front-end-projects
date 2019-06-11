fb137e125aa619d0d9447b278aadf845226b81f9
ninaderi <nina8867@gmail.com>
2019-06-11 10:12:34 -0600

    robo commit

---
+++ src/SearchBox.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SearchBox = ({searchChange}) => { // We are using destructuring here: ({..}) which allows us to grab the props object and grab its properties.
+	return (
+		<div className='pa2'>
+			<input 
+				className='pa3 ba b--green bg-lightest-blue'
+				type='search' 
+				placeholder='search robots' 
+				onChange={searchChange}
+			/>
+		</div>
+	);
+}
+
+
+export default SearchBox
